48f385599a28d52b3d570e8658d0bf4e6292e30b
h67pur <alexlunegov52@gmail.com>
2025-01-09 12:42:22 +0300

    Дописал первый тест. Добавил проверку на целостность созданных данных через фабрику

---
+++ app/tests/Feature/GetAllProductsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class GetAllProductsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_index_returns_all_products()
+    {
+        $products = Product::factory()->count(1000)->create();
+
+        $response = $this->getJson('/products');
+        $response->assertStatus(200);
+        $response->assertJsonCount(1000);
+        $responseData = $response->json();
+        foreach ($products as $product) {
+            $this->assertTrue(collect($responseData)->contains('id', $product->id),);
+            $returnedProduct = collect($responseData)->firstWhere('id', $product->id);
+            $this->assertEquals($product->name, $returnedProduct['name']);
+            $this->assertEquals($product->price, $returnedProduct['price']);
+            $this->assertEquals($product->description, $returnedProduct['description']);
+        }
+    }
+}
