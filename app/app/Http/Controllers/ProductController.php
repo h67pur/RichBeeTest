5cf66be822549cdaf7accee09782abae7381c04b
h67pur <alexlunegov52@gmail.com>
2025-01-09 11:25:38 +0300

    Модели и их фабрики

---
+++ app/app/Http/Controllers/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductCategory;
+
+class ProductController extends Controller
+{
+    public function index(): array
+    {
+        return [];
+    }
+}
